6aa900f9fb793c242b8323bef631a35145a51339
Marharyta Filipovych <mfilipovych@griddynamics.com>
2026-09-23 21:42:45 +0300

    initial setup and config

---
+++ src/test/java/com/margo/useractivitylogsystem/UseractivitylogsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margo.useractivitylogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseractivitylogsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
